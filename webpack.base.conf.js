6109e536c2e32b54575f13c644e836c3f75fbcc8
Raphael Aleixo <raphael.aleixo@gmail.com>
2018-09-06 20:59:16 -0300

    Adjusting webpack to deal with local fonts.

---
+++ webpack.base.conf.js
@@ -0,0 +1,8 @@
+ {
+    test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+    loader: 'url-loader',
+    options: {
+      limit: 10000,
+      name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+    }
+  },
