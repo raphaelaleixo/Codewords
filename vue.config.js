74c7ecbc6ed7be6b994de722dc91573de430301f
Raphael Aleixo <raphael.aleixo@gmail.com>
2018-09-10 20:52:40 -0300

    Adjusting manifest

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   configureWebpack: {
-  entry: ["babel-polyfill", "./src/main.js"]
+    entry: ["babel-polyfill", "./src/main.js"]
+  },
+  pwa: {
+    name: 'Codewords',
+    themeColor: '#000000',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
   }
-  }
+}
